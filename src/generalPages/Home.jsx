5e5299eee0b0150cbfeb273332f96f2f839d66d5
AbdulrhamnAhmed29 <abdulrhmanaahmed@gmail.com>
2026-02-27 14:50:12 +0200

    feat(ui): add shared UI components and pages structure

---
+++ src/generalPages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeroSection from '../generalsComponents/home/HeroSection'
+import MenuSection from '../features/products/component/MenuSection'
+import DigitalMenu from '../features/products/component/DigitalMenu'
+
+function Home() {
+    return (
+        <div>
+            <HeroSection />
+            <MenuSection />
+            <DigitalMenu />
+        </div>
+    )
+}
+
+export default Home
