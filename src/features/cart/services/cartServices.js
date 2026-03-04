fc17aaf08cbe7e629274632fe8df43621ba55c75
AbdulrhamnAhmed29 <abdulrhmanaahmed@gmail.com>
2026-03-04 11:18:13 +0200

    Implement cart services and hook logic

--- src/features/cart/services/cartServices.js
+++ src/features/cart/services/cartServices.js
@@ -1,1 +1,24 @@
+import { baseApi } from "../../../api/baseApi"
 
+export const cartServices = {
+    // get all cart data 
+    getAllCart: async () => {
+        const res = await baseApi.getAll("/cart-items");
+        return res.data
+    },
+
+    create: async (data) => {
+        const res = await baseApi.create("/cart-items", data);
+        return res.data 
+    },
+
+    deleteCartData: async (id) => {
+        const res = await baseApi.delete("/cart-items", id);
+        return res.data
+    },
+    updateData: async (id, quantity) => {
+        const res = await baseApi.update("/cart-items", id);
+        return res.data
+    }
+
+}
