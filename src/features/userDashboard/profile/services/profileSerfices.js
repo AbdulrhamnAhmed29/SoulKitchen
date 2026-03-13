7e3a814a69eea4917d50ec1e1cdf1d502231f7ce
AbdulrhamnAhmed29 <abdulrhmanaahmed@gmail.com>
2026-03-13 09:51:16 +0200

    fix: update profile services

--- src/features/userDashboard/profile/services/profileSerfices.js
+++ src/features/userDashboard/profile/services/profileSerfices.js
@@ -1,8 +1,8 @@
 import { baseApi } from "../../../../api/baseApi"
 
- export const profileServices ={
-getCurrantUser:async()=>{
-    const res =await baseApi.getAll("users/me?populate=*");
-    return res
-}
+export const profileServices = {
+    getCurrantUser: async () => {
+        const res = await baseApi.getAll("users/me?populate=*");
+        return res
+    }
 }
